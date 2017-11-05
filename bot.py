8f3388e2d48dab44266bcf44f2c9518682f458a2
Rowan <LargeBee@users.noreply.github.com>
2017-11-05 17:39:31 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ else:
 
 starttime=time.time()
 #in seconds
-interval = settings['RSS Info']['interval']
+interval = float(settings['RSS Info']['interval'])
 while True:
     feed = feedparser.parse(rss_url)
     print("Checking for new articles!")
