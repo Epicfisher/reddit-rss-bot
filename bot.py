8d2fab25579e8e8ade11886823267804c0b89211
Dan <Epicfisher@users.noreply.github.com>
2018-04-22 16:48:27 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,41 +26,33 @@ post_interval = float(os.environ['POSTINTERVAL'])
 #       postedArticles = postedArticles.split("\n")
 #       postedArticles = list(filter(None, postedArticles))
 
-#postedArticles = reddit.front.new()
+#newPosts = reddit.front.new()
 postedArticlesGenerated = reddit.redditor('TeignmouthNews').submissions.new()
 postedArticles = list(postedArticlesGenerated)
 
-#for post in postedArticles:
-#    print(post.url.replace("http://", "https://"))
-
-linkInPosted = 0
+linkInPosted = False
 
 starttime=time.time()
 while True:
     feed = feedparser.parse(rss_url)
-    print("Checking for new articles!")
+    print("Now checking for new articles!")
     newArticles = 0
     for item in feed['items']:
-        linkInPosted = 0
-        i = 0
-        for articleLink in postedArticles:
-            i = i + 1
-        print(str(i) + " Elements in postedArticles")
+        linkInPosted = False
         for articleLink in postedArticles:
             #print("Checking '" + item['link'].replace("http://", "https://") + "' and '" + articleLink.url.replace("http://", "https://") + "'")
-            #print("Checking '" + articleLink.url + "'")
             if item['link'].replace("http://", "https://") == articleLink.url.replace("http://", "https://"):
-                linkInPosted = 1
+                linkInPosted = True
                 #print(item['link'].replace("http://", "https://") + " _ IS EQUAL TO _ " + articleLink.url.replace("http://", "https://"))
             #else:
                 #print(item['link'].replace("http://", "https://") + " _ IS NOT EQUAL TO _ " + articleLink.url.replace("http://", "https://"))
-        if linkInPosted == 0:
+        if linkInPosted == False:
             #medial_url = item['media_content'][0]['url']
             if debug == "0":
-                subreddit.submit(item['title'], url=item['link'])
-                print("Posted '" + item['title'] + "' at Link '" + item['link'] + "'")
+                subreddit.submit(item['title'], url=item['link'].replace("http://", "https://"))
+                print("Posted '" + item['title'] + "' at Link '" + item['link'].replace("http://", "https://") + "'")
             else:
-                print("(DEBUG) Would have Posted '" + item['title'] + "' at Link '" + item['link'] + "'")
+                print("(DEBUG) Would have Posted '" + item['title'] + "' at Link '" + item['link'].replace("http://", "https://") + "'")
             newArticles += 1
             time.sleep(post_interval)
         else:
@@ -69,6 +61,6 @@ while True:
             else:
                 print("Skipped '" + item['title'] + "' at Link '" + item['link'] + "'")
 
-    print("{0} new articles were posted. Now waiting...".format(newArticles))
+    print("{0} new articles were posted. Now waiting {1} seconds...".format(newArticles, interval))
 
     time.sleep(interval - ((time.time() - starttime) % interval))
